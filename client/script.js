645f2d6bb47b3031e90dc0b5c7e0351a1f85e64e
karma007 <abhishekpandey.kr@gmail.com>
2023-03-04 11:08:16 +0530

    code updated for endpoint

--- client/script.js
+++ client/script.js
@@ -68,7 +68,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId)
   loader(messageDiv)
   // fetch data from server
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://chatgpt3-lceq.onrender.com/", {
     method:"POST",
     headers:{
       "Content-Type":"application/json"
